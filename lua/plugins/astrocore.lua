a60f86e2f7fb3056f4cd246b6307d3b0dfa2e52e
winzu44 <ftakuabai5000@gmail.com>
2024-08-18 09:44:49 +0900

    Fix : toggle term command with F7 key

--- lua/plugins/astrocore.lua
+++ lua/plugins/astrocore.lua
@@ -64,15 +64,22 @@ return {
         ["<Home>"] = {"^"},
         
         -- buffer control
-        ["<C-8>"] = {":bprevious<CR>"},
-        ["<C-7>"] = {":bnext<CR>"},
+        ["<C-7>"] = {":bprevious<CR>"},
+        ["<C-8>"] = {":bnext<CR>"},
 
+        
+        ["<F7>"] = {":ToggleTerm direction=float<CR>"},
+        
+        
         -- tables with just a `desc` key will be registered with which-key if it's installed
         -- this is useful for naming menus
         -- ["<Leader>b"] = { desc = "Buffers" },
 
         -- setting a mapping to false will disable it
         -- ["<C-S>"] = false,
+      },
+      i = {
+        
       },
     },
   },
